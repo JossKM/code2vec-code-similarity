55d526eb1c6d30850790eb6bcaa15ee98998ec20
JossKM <31990727+JossKM@users.noreply.github.com>
2022-04-08 17:20:06 -0400

    Updated gitignore to ignore outputs

--- inputs/Input.java
+++
@@ -1,7 +0,0 @@
-int faaa(int n) {
-    if (n == 1) {
-        return 1; 
-    } else {
-        return n * f(n-2);
-    }
-}
--- inputs/input2.java
+++
@@ -1,7 +0,0 @@
-int q(int naaa) {
-    if (naaa == 1) {
-        return 1; 
-    } else {
-        return naaa * f(naaa-1);
-    }
-}
